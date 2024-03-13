346f4373b8acc1f17e58a93b45de67ae5a28dbbd
devlrakib <devrakib5@gmail.com>
2024-03-13 17:05:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// ! Get Element By Index
+var numbers = [45, 68, 50, 40, 20];
+var elements = numbers[1]
+console.log(elements);
+//console.log(numbers[0]);
+
+// ? Set Element Value By Index
+var price1 = [20, 30, 40, 50];
+price1[2] = 70;
+console.log(price1);
+
+// ! Find Index Of An Elements
+var price2 = [80, 70, 90, 50];
+var positionFixed = price2.indexOf(50);
+console.log(positionFixed);
